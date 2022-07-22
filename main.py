c19a8bd8aa9e7dd763f2a45a36294238905d4b9d
KHADIJA NAZ <67429399+KhadijaNaz@users.noreply.github.com>
2022-07-22 21:43:50 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from ast import main
+import string
+if __name__=="__main__":
+    p1=string.ascii_lowercase
+    #print(p1)
+    p2=string.ascii_uppercase
+    #print(p2)
+    p3=string.digits
+    #print(p3)
+    p4=string.punctuation
+    #print(p4)
+    passlen=int(input("Enter password length"))
